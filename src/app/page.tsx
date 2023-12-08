6cb5e8ab9219d2b1277fb5cce011e6327eef821a
0xshikhar <64684053+0xshikhar@users.noreply.github.com>
2023-12-09 00:42:14 +0530

    added livepeer client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,6 @@ export default function Home() {
   return (
     <div>
       <Hero />
-      <div className="container mx-auto">
-        <h1 className="text-4xl font-bold text-center">Hello World</h1>
-        <Image src="/images/hero.jpg" width={300} height={300} />
-    </div>
     </div>
   )
 }
